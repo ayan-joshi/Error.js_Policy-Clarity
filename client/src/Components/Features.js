f532531328519cecae8b3f7551333a0dcff42a54
Aryan Singh <aryansingh6011@gmail.com>
2024-01-21 15:59:42 +0530

    final website

--- client/src/Components/Features.js
+++ client/src/Components/Features.js
@@ -5,6 +5,7 @@ const Features = () => {
   const fileInputRef = useRef(null);
   const [selectedFileName, setSelectedFileName] = useState(null);
   const [pdfContent, setPdfContent] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleFileChange = (selectedFiles) => {
     if (selectedFiles.length > 0) {
@@ -41,6 +42,8 @@ const Features = () => {
 
   const handleUpload = () => {
     if (selectedFileName) {
+      setIsLoading(true);
+
       const apiUrl = "http://127.0.0.1:8000/uploadfile/";
 
       const formData = new FormData();
@@ -58,6 +61,9 @@ const Features = () => {
         })
         .catch((error) => {
           console.error("Error:", error);
+        })
+        .finally(() => {
+          setIsLoading(false);
         });
     } else {
       console.log("Please select a file");
@@ -81,55 +87,87 @@ const Features = () => {
   };
 
   const handleDeleteFile = () => {
+    setIsLoading(false);
     setSelectedFileName(null);
     setPdfContent(null);
   };
 
   return (
-    <div id='policy' className="flex justify-center items-center flex-col py-24 gap-8 z-50 bg-gradient-to-r from-white-1200 via-white-600 to-white-400 ..." onDragOver={handleDragOver} onDragEnter={handleDragEnter} onDrop={handleDrop}>
-    <p className="text-7xl font-medium">Upload Policies</p>
-    <p className="lg:w-3/5 w-full px-12 text-center lg:text-xl text-xs font-normal ">
-    This process facilitates convenient access,<br>
-    </br> verification, and 
-    management of health insurance information
-        </p>
-    <div className="flex justify-center items-center rounded-md border-dashed border max-w-4xl w-full border-gray-500 px-6 py-20 cursor-pointer" onClick={handleLabelClick}>
-      <input type="file" className="hidden" onChange={handleFileInputChange} ref={fileInputRef} />
-      <div className="flex flex-col justify-center items-center gap-2">
-        {selectedFileName ? (
-          ""
-        ) : (
-          <span className="">
-            <img width="40" height="25" src="https://img.icons8.com/ios/5000/plus--v1.png" alt="plus--v1" />
-          </span>
-        )}
-        <div className="flex flex-col justify-center items-center gap-5">
-          <span className="ml-2 text-lg">{selectedFileName && `File Name: ${selectedFileName}`}</span>
+    <div
+      id="policy"
+      className="flex justify-center items-center flex-col py-24 gap-8 z-50 bg-gradient-to-r from-white-1200 via-white-600 to-white-400 ..."
+      onDragOver={handleDragOver}
+      onDragEnter={handleDragEnter}
+      onDrop={handleDrop}
+    >
+      <p className="text-7xl font-medium">Upload Policies</p>
+      <p className="lg:w-3/5 w-full px-12 text-center lg:text-xl text-xs font-normal ">
+        This process facilitates convenient access,<br></br> verification, and
+        management of health insurance information
+      </p>
+      <div
+        className="flex justify-center items-center rounded-md border-dashed border max-w-4xl w-full border-gray-500 px-6 py-20 cursor-pointer"
+        onClick={handleLabelClick}
+      >
+        <input
+          type="file"
+          className="hidden"
+          onChange={handleFileInputChange}
+          ref={fileInputRef}
+        />
+        <div className="flex flex-col justify-center items-center gap-2">
           {selectedFileName ? (
             ""
           ) : (
-            <span className="text-gray-600 text-lg text-center">
-              Click to upload file or Drag <br /> and drop your files
+            <span className="">
+              <img
+                width="40"
+                height="25"
+                src="https://img.icons8.com/ios/5000/plus--v1.png"
+                alt="plus--v1"
+              />
             </span>
           )}
-          {selectedFileName ? (
-            <div className="flex flex-row justify-center items-center gap-6">
-              <button type="button" className="bg-black px-6 py-2 text-white rounded-md" onClick={handleUpload}>
-                Upload
-              </button>
-              <button type="button" className="px-6 py-2 border border-black rounded-md" onClick={handleDeleteFile}>
-                Cancel
-              </button>
-            </div>
-          ) : (
-            ""
-          )}
+          <div className="flex flex-col justify-center items-center gap-5">
+            <span className="ml-2 text-lg">
+              {selectedFileName && `File Name: ${selectedFileName}`}
+            </span>
+            {selectedFileName ? (
+              ""
+            ) : (
+              <span className="text-gray-600 text-lg text-center">
+                Click to upload file or Drag <br /> and drop your files
+              </span>
+            )}
+            {selectedFileName ? (
+              <div className="flex flex-row justify-center items-center gap-6">
+                <button
+                  type="button"
+                  className="bg-black px-6 py-2 text-white rounded-md"
+                  onClick={handleUpload}
+                >
+                  Upload
+                </button>
+                <button
+                  type="button"
+                  className="px-6 py-2 border border-black rounded-md"
+                  onClick={handleDeleteFile}
+                >
+                  Cancel
+                </button>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
       </div>
+      {isLoading && (
+        <div className="animate-spin rounded-full border-t-4 border-blue-500 border-opacity-25 h-12 w-12 mt-4"></div>
+      )}
+      {pdfContent && <button onClick={handleDownload}>Download PDF</button>}
     </div>
-    {pdfContent && <button onClick={handleDownload}>Download PDF</button>}
-  </div>
-);
+  );
 };
 
 export default Features;
--- client/src/Components/Hero.js
+++ client/src/Components/Hero.js
@@ -1,33 +1,27 @@
-
 import React, { useState } from "react";
-
-
+import Lottie from "react-lottie";
+import animationData from "../assets/main.json"; // Adjust the path based on your project structure
 
 const Hero = () => {
- 
   return (
-    <div className="flex justify-center relative items-center flex-col lg:gap-8 gap-5 lg-pt-0  pt-6  bg-gradient-to-r from-white-900 via-white-600 to-white-400 ...">
-      
-      
+    <div className="flex justify-evenly h-[70vh] relative items-center flex-row lg:gap-8 gap-5 lg-pt-0 pt-6 bg-gradient-to-r from-white-900 via-white-600 to-white-400 ...">
       {/* <Logo/> */}
-      <div >
-      <img src="https://img.icons8.com/ios-filled/50/000000/caduceus.png" alt="caduceus"/>
+      <div className="left-52 w-60 lg:block absolute hidden -top-60 ">
+        <Lottie options={{ animationData }} height={200} width={200} />
       </div>
-      <div className="left-52 lg:block absolute hidden -top-60 ">
-   
-      </div >
-      <div className="flex justify-center items-center lg:gap-8 gap-[20px] flex-col z-20" >
-        {/* <p className='text-primary lg:text-lg  text-sm font-medium lg:leading-7 leading-5'>Some Title Here</p> */}
+      <div className="flex justify-center items-center w-[40vw] lg:gap-8 gap-[20px] flex-col z-20">
         <p className="text-black text-center lg:text-6xl lg:tracking-[-2px] tracking-normal sm:text-3xl text-xl font-medium lg:w-[60%] w-2/3 lg:leading-[60px] leading-7 py-5">
           Summarization of <br />
           Health Insurance Policies
         </p>
         <p className="lg:w-3/5 w-full px-12 text-center lg:text-xl text-xs font-normal  ">
-          We Summarize the policies for individuals to help them 
-          prevent Scams and explain them policies .
+          We Summarize the policies for individuals to help them prevent Scams
+          and explain them policies.
         </p>
-       
-
+      </div>
+      <div className="w-[40vw] justify-center flex">
+        {/* Lottie animation instead of the image */}
+        <Lottie options={{ animationData }} height={1000} width={1000} />
       </div>
     </div>
   );
